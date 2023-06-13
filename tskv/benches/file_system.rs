14bd2834571d0103889f80e6a12e82c382e0e3a6
Subsegment <304741833@qq.com>
2023-06-13 16:15:27 +0800

    test: file system test of io uring and std fs

--- tskv/benches/file_system.rs
+++ tskv/benches/file_system.rs
@@ -8,7 +8,7 @@ use rand::Rng;
 
 
 async fn run_iouring(id: u64, data: &[u8]) {
-    let path = format!("/tmp/test{}.txt",id);
+    let path = format!("/data1/syk/tmp0/test{}.txt",id);
     let mut file = FileCursor::from(create_file(path).await.unwrap());
     file.write(&data).await.unwrap();
 }
@@ -20,9 +20,9 @@ fn test_iouring(c: &mut Criterion) {
         .map(u8::from)
         .collect::<Vec<u8>>();
     let mut id = 0;
+    let rt = Runtime::new().unwrap();
     c.bench_function("io_uring", |b| {
         b.iter(|| {
-            let rt = Runtime::new().unwrap();
             rt.block_on(run_iouring(id, &data));
             id += 1;
         })
@@ -30,7 +30,7 @@ fn test_iouring(c: &mut Criterion) {
 }
 
 fn run_direct(id: u64, data: &[u8]) {
-    let path = format!("/tmp/test{}.txt",id);
+    let path = format!("/data1/syk/tmp1/test{}.txt",id);
     let mut file = std::fs::File::create(path).unwrap();
     file.write_all(&data).unwrap();
 }
